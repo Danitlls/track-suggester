2339ad33c3fab5a998af15432715157437bdc1ef
Daniela Trulls <danielatrulls01@gmail.com>
2017-04-28 10:40:06 -0700

    Add links to bootstrap and Jquery in index.html, add functions to colect values from user in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+  $("#quiz").submit(function(event){
+    event.preventDefault();
 
+    var detail = $("input:radio[name=detail]:checked").val();
+    var logic = $("input:radio[name=logic]:checked").val();
+    var company = $("input:radio[name=company]:checked").val();
+    var art = $("input:radio[name=art]:checked").val();
+    var lenguage = [];
+    $.each($("input[name=Options]:checked"), function(){
+        lenguage.push($(this).val());
+
+    });
+    console.log(lenguage);
+    // alert("My favourite lenguages are: " + lenguage.join(", "));
+
+
+  });
+});
